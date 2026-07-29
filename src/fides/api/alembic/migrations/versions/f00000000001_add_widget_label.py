a03684cb8c4614a11678cef4edf5c6eb453cd9c9
Adrian Galvan <adriang430@gmail.com>
2026-07-28 21:34:22 -0700

    fix: add widget.label (#64)
    Co-authored-by: t <a@b.c>

---
+++ src/fides/api/alembic/migrations/versions/f00000000001_add_widget_label.py
@@ -0,0 +1,23 @@
+"""Add widget.label
+
+Revision ID: f00000000001
+Revises: e00000000001
+Create Date: 2026-05-02 09:00:00.000000
+
+"""
+
+import sqlalchemy as sa
+from alembic import op
+
+revision = "f00000000001"
+down_revision = "e00000000001"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column("widget", sa.Column("label", sa.String(), nullable=True))
+
+
+def downgrade():
+    op.drop_column("widget", "label")
